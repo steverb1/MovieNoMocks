498f0985a2a51893a03a5d04d15321e6fbc769de
steve.bement <steve.bement@accenture.com>
2022-05-26 12:46:02 -0600

    Dao test uses real database

--- src/test/java/com/aba/nomocks/dao/MovieDaoTest.java
+++ src/test/java/com/aba/nomocks/dao/MovieDaoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class MovieDaoTest {
 	@Test
 	public void testInsertMovie() {
-		MovieDao movieDao = MovieDao.createNull();
+		MovieDao movieDao = MovieDao.create();  // Real dao, just to see if mongodb is working.
 		movieDao.insertMovie("The Princess Bride", 1987);
 	}
 }
