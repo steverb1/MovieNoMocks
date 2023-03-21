bb6e466145e71d31864a1f09b28e8a7613d9aa91
steve.bement <steve.bement@accenture.com>
2023-03-21 09:34:44 -0600

    Adding nullable test at Dao level, and demo instructions.

---
+++ src/test/java/com/aba/nomocks/dao/MovieDaoTest.java
@@ -0,0 +1,19 @@
+package com.aba.nomocks.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.aba.nomocks.biz.Movie;
+
+public class MovieDaoTest {
+	@Test
+	public void testSaveMovie() {
+		MovieDaoMongo movieDao = MovieDaoMongo.createNull();
+		
+		Movie movie = new Movie("test", 1984);
+		String id = movieDao.saveMovie(movie);
+		
+		assertEquals("test", movieDao.getLastWrite().title);
+	}
+}
